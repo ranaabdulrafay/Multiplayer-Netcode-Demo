4ec3bdb380e909887cef91a38c05760360e530af
ranaabdulrafay <ranaabdulrafay@hotmail.com>
2023-01-24 18:35:15 +0500

    added a custom data

--- Multiplayer NetCode/Assets/Scripts/PlayerMovement.cs
+++ Multiplayer NetCode/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using static Unity.Networking.Transport.NetworkDriver;
 
+// this must be of passing by values
+[System.Serializable]
+public class MyNetworkData : INetworkSerializable
+{
+    public Vector3 HitPoint;
+
+    public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+    {
+        serializer.SerializeValue(ref HitPoint);
+    }
+}
 public class PlayerMovement : NetworkBehaviour
 {
     RaycastHit hit;
     Vector3 pos = Vector3.zero;
+
+    // network
+    public NetworkVariable<MyNetworkData> NetworkHitData = new NetworkVariable<MyNetworkData>(new MyNetworkData()
+        , NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+    public void ChangeData(MyNetworkData prev , MyNetworkData newValue)
+    {
+        Debug.Log("value changed ");
+        pos = newValue.HitPoint;
+    }
+    public override void OnNetworkSpawn()
+    {
+        NetworkHitData.OnValueChanged += ChangeData;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +47,12 @@ public class PlayerMovement : NetworkBehaviour
         {
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                pos = hit.point;
+                MyNetworkData data = new MyNetworkData();
+                data.HitPoint = hit.point;
+                NetworkHitData.Value = data;
             }
         }
 
-        transform.position = Vector3.Lerp (transform.position,pos,Time.deltaTime*2);
+        transform.position = Vector3.Lerp (transform.position, pos, Time.deltaTime*2);
     }
 }
